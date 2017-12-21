b5fffa4a362c6602bb57a2b8383a0516e5ec4591
Larry Custodio <custodioly@gmail.com>
2017-12-21 13:42:52 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: path.join(__dirname, '/src'),
+  devtool: 'source-maps',
+  entry: {
+    javascript: './js/index'
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/dist'),
+  },
+
+  resolve: {
+    alias: {
+      react: path.join(__dirname, 'node_modules', 'react')
+    },
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]',
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+};
